6aa3e41c55f41f161a69578c92f2debf48c9dfea
nicopleyto <nicopleyto@gmail.com>
2022-05-12 21:15:40 +0800

    finished customers controller

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,28 +1,25 @@
 class CustomersController < ApplicationController
-  before_action :set_customer, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_user!, except: [:new, :create]
+  before_action :set_custom_create, only: [:create]
+  before_action :authenticate_admin, only: [:admin_index]
 
-  # GET /customers
-  def index
+  def admin_index
     @customers = Customer.all
   end
 
-  # GET /customers/1
-  def show
+  def index
+    @customers = current_user.customers
   end
 
-  # GET /customers/new
   def new
-    @customer = Customer.new
+    if user_signed_in?
+      @customer = current_user.customers.build
+    else
+      @customer = Customer.new
+    end
   end
 
-  # GET /customers/1/edit
-  def edit
-  end
-
-  # POST /customers
   def create
-    @customer = Customer.new(customer_params)
-
     if @customer.save
       redirect_to @customer, notice: 'Customer was successfully created.'
     else
@@ -30,29 +27,27 @@ class CustomersController < ApplicationController
     end
   end
 
-  # PATCH/PUT /customers/1
-  def update
-    if @customer.update(customer_params)
-      redirect_to @customer, notice: 'Customer was successfully updated.'
-    else
-      render :edit
-    end
-  end
-
-  # DELETE /customers/1
-  def destroy
-    @customer.destroy
-    redirect_to customers_url, notice: 'Customer was successfully destroyed.'
-  end
-
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_customer
-      @customer = Customer.find(params[:id])
-    end
 
-    # Only allow a list of trusted parameters through.
-    def customer_params
-      params.require(:customer).permit(:name, :email, :mobile_number)
+  # Only allow a list of trusted parameters through.
+  def customer_params
+    params.require(:customer).permit(:name, :email, :mobile_number, :store_name)
+  end
+
+  def set_custom_create
+    if user_signed_in?
+      @customer = current_user.customers.build(customer_params)
+    else
+      @customer = Customer.new(customer_params)
     end
+  end
+
+  def authenticate_admin
+    if current_user.admin?
+      return
+    else
+      redirect_to root_path, notice: "You must be an admin to perform this action."
+    end
+  end
+
 end
