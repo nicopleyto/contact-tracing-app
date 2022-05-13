be460d7192c68897d88e331f96814bd818e51408
nicopleyto <nicopleyto@gmail.com>
2022-05-13 17:28:02 +0800

    added seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,7 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-User.create(
+User.create!(
     email: 'admin@mall.com', 
     store: 'mall admin', 
     password: '123456', 
