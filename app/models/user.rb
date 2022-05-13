eb5c9e0ed7da8f7a752feeb9a97e62a396d3d05d
nicopleyto <nicopleyto@gmail.com>
2022-05-13 18:06:24 +0800

    added body styling, shared directory, removed unnecesssary views from customers

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  has_many :customers
+         
+  has_many :customers, dependent: :destroy
   validates :name, presence: true
   validates :email, presence: true
   validates :password, presence: true
