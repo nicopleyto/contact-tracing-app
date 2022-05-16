0838169e229b0c2f9a68d26c3f243d3bdbffd6e9
nicopleyto <nicopleyto@gmail.com>
2022-05-16 23:44:33 +0800

    styled admin login

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,7 +12,10 @@ module.exports = {
     extend: {},
   },
   variants: {
-    extend: {},
+    extend: {
+      backgroundColor: ['checked'],
+      borderColor: ['checked'],
+    },
   },
   plugins: [],
 }
